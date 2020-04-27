f09078712c32c5f45a1d7863e61d3c33a1b912da
Peter Mercado <peter@Peters-MacBook-Pro.local>
2020-04-27 14:48:52 -0700

    express server set up with initial routes

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+
+app.get('/', (req, res) => res.json({ msg: 'welcome to the contact keeper' }));
